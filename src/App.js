201479eb85b862df1ba688b8f83c431162601f2e
Aniket Patel <patelaniket1207@gmail.com>
2022-12-29 03:18:21 +0530

    added secondpage structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './assets/Css/App.css';
 import Homepage from './Pages/Homepage.jsx';
+import Secondpage from './Pages/Secondpage';
 
 function App() {
   return (
     <>
       App .js
-      <Homepage />
+      {/* <Homepage /> */}
+      <Secondpage />
     </>
   );
 }
